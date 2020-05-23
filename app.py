13caf1a18e9656b2ee90492eb73e7ef85f07df7b
obiwankubernetes <samrperl@gmail.com>
2020-05-22 22:47:38 -0400

    thru 24:30

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+# import libraries
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+# configure flask app
 app = Flask(__name__)
+
+# configure SQLite DB
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mnt/C:/Users/sampe/OneDrive/Code Projects/blog-flask/blogdb.db"
+db = SQLAlchemy(app)
 
 # first route for home page and show user template index.html when goes to this url
 @app.route('/')
@@ -19,7 +26,7 @@ def post():
 
 # 4th route
 @app.route('/contact')
-def add():
+def contact():
     return render_template('contact.html')
 
 # code to run app
